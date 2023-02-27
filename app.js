1a1f42ff20966ec95a56d13567f12123ad81b8b9
Richard Guilliams <dropletswebdevelopment@gmail.com>
2023-02-27 16:16:00 -0700

    Prepared App for Deployment

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -25,7 +26,15 @@ app.enable('trust proxy')
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
+
+
 //Global Middleware
+// Implementing cors
+app.use(cors());
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors());
+// Access-Control-Allow-Origin *
+
 app.use(express.static(path.join(__dirname, '/public')));
 // Data Security
 app.use(helmet());
