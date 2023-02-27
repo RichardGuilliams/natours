4f6eb251f297cc119210241c46466c52f56a9484
Richard Guilliams <dropletswebdevelopment@gmail.com>
2023-02-27 14:40:43 -0700

    Prepared App for Deployment

--- controllers/authController.js
+++ controllers/authController.js
@@ -82,6 +82,7 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
+    console.log('split');
     token = req.headers.authorization.split(' ')[1];
   } else if (req.cookies.jwt) {
     token = req.cookies.jwt;
