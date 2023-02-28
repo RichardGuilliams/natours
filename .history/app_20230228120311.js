db87d5e0ffe07f51d4ecf81335fa40c32f1e0560
Richard Guilliams <dropletswebdevelopment@gmail.com>
2023-02-28 12:03:36 -0700

    Improved Stripe implementation

--- .history/app_20230228120311.js
+++ .history/app_20230228120311.js
@@ -71,9 +71,10 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-// app.post('/webhook-checkout',
-//     express.raw({ type: 'application/json'}),
-//     bookingController.webhookCheckout);
+app.post('/webhook-checkout',
+    express.raw({ type: 'application/json'}),
+    bookingController.webhookCheckout
+    );
 
 //Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
